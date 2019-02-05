0f8a3913f8ba24e39235bd818247b430c57fd5a4
smet_k <ski97@mail.ru>
2019-02-05 11:52:30 +0300

    rle compress and some tests

--- source/max.js
+++ source/max.js
@@ -1,3 +1,24 @@
 'use strict';
 
 const max = numbers => Math.max(...numbers);
+
+const rle = function (string) {
+    var tmp = string[0]
+    var counter = 1
+    var result = ''
+    for (var i = 1; i <= string.length; i++) {
+        if (string[i] == tmp) {
+            counter++
+        } else {
+            if (counter != 1) {
+                result += (tmp + counter)
+            } else {
+                result += tmp
+            }
+            tmp = string[i]
+            counter = 1
+        }
+    }
+
+    return result
+}
--- tests/rle.js
+++ tests/rle.js
@@ -5,5 +5,10 @@ QUnit.module('Тестируем функцию rle', function () {
 		assert.strictEqual(rle('AAAB'), 'A3B');
 		assert.strictEqual(rle('BCCDDDAXXXX'), 'BC2D3AX4');
 		assert.strictEqual(rle('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD'), 'AV3B3V2XDHJF4D6HA4J3D2SLS3D4');
+		assert.strictEqual(rle('AAAAAAAABCCCC'), 'A8BC4');
+		assert.strictEqual(rle('YYYYYYYYYTRR'), 'Y9TR2');
+		assert.strictEqual(rle('WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW'), 'W12BW12B3W24BW14')
+		assert.strictEqual(rle('Xtmprsqzntwlfb'), 'Xtmprsqzntwlfb');
+		assert.strictEqual(rle('AAAAAAbbbXXXXXt'), 'A6b3X5t')
 	});
 });
