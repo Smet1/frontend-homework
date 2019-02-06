5b0be2fffd2a1a2ca2a0297e2851221b86e7a3f4
smet_k <ski97@mail.ru>
2019-02-06 12:36:08 +0300

    added ';'

--- source/max.js
+++ source/max.js
@@ -4,23 +4,25 @@ const max = numbers => Math.max(...numbers);
 
 // const rle = function (string) {
 const rle = string => {
-    var tmp = string[0]
-    var counter = 1
-    var result = ''
-    
+    if (typeof(string) !== "string") return undefined;
+
+    var tmp = string[0];
+    var counter = 1;
+    var result = "";
+
     for (var i = 1; i <= string.length; i++) {
-        if (string[i] == tmp) {
-            counter++
+        if (string[i] === tmp) {
+            counter++;
         } else {
             if (counter != 1) {
-                result += (tmp + counter)
+                result += (tmp + counter);
             } else {
-                result += tmp
+                result += tmp;
             }
-            tmp = string[i]
-            counter = 1
+            tmp = string[i];
+            counter = 1;
         }
     }
 
-    return result
+    return result;
 }
